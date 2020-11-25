724d3dea863c728755f087e711bb25c6cc67a1eb
Silviu Iordache <silviu.iordache@caphyon.com>
2020-11-25 21:58:37 +0200

    Create dummy search bar.

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .search-bar.
+    Search
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  methods: {
+
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+  .card-item
+    color white
+
+</style>
