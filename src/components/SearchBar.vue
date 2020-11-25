abf7c95849de6c19ded935c1beccf1c4acec213d
Silviu Iordache <silviu.iordache92@gmail.com>
2020-11-25 22:13:35 +0200

    Polish the search bar.

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,19 +1,32 @@
 <template lang="pug">
-  .search-bar.
-    Search
+  .search-bar
+    input#search-input(type="text" :placeholder="placeholderText" @focus="removePlaceholder()" @blur="addPlaceholder()")
 </template>
 
 <script>
 export default {
   name: 'SearchBar',
+  data() {
+    return {
+      placeholderText: 'Cube Stereo..'
+    }
+  },
   methods: {
-
+    addPlaceholder() {
+      document.getElementById('search-input').placeholder = this.placeholderText;
+    },
+    removePlaceholder() {
+      document.getElementById('search-input').placeholder = '';
+    }
   }
 };
 </script>
 
 <style lang="stylus" scoped>
-  .card-item
-    color white
+  #search-input
+    width 25rem
+    height 3rem
+
+
 
 </style>
