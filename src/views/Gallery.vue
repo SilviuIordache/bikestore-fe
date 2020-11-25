7d492d5cd2d97a4f3fa9aa6f86129ea43b3bccae
Silviu Iordache <silviu.iordache92@gmail.com>
2020-11-25 21:58:28 +0200

    Remove garbage.

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -3,27 +3,21 @@
     .gallery-banner.py-3
       h1 Welcome to the Bike Store
       p Browse, find and buy your ideal bike
-    card-item-list(:items="products")
+    search-bar
+    card-item-list
 </template>
 
 <script>
-
 import CardItemList from '../components/CardItemList.vue';
-import products from '../assets/products.json';
+import SearchBar from '../components/SearchBar.vue';
 
 export default {
   name: 'Gallery',
   components: {
     'card-item-list': CardItemList,
+    'search-bar': SearchBar
   },
-  data() {
-    return {
-      products,
-    };
-  },
-  mounted() {
-    this.products = products.products;
-  },
+
 };
 </script>
 
