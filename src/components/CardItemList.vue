61582458befa1be2f691015a4a31ce871aa8ac57
Silviu Iordache <silviu.iordache92@gmail.com>
2020-11-26 18:04:57 +0200

    Move request to gallery.

--- src/components/CardItemList.vue
+++ src/components/CardItemList.vue
@@ -18,27 +18,15 @@ export default {
   components: {
     'card-item': CardItem,
   },
+  props: {
+    items: {
+      type: Array,
+      default: []
+    }
+  },
   data() {
     return {
-      items: [],
     };
-  },
-  async created() {
-    this.getBikes();
-  },
-  methods: {
-    async getBikes() {
-      const url = 'http://localhost:3000/api/v1/bikes';
-      let response;
-      try {
-        response = await this.$http.get(url);
-      } catch (err) {
-        console.log(`Error retrieving items: ${err}`)
-      }
-      if (response) {
-        this.items = response.data.data.bikes;
-      }
-    },
   },
 };
 </script>
